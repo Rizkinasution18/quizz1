43ada2892102635a75235e82dcf8de1aea7ac06a
Rizkinasution18 <rizkiprastya.h.nst@gmail.com>
2023-12-13 09:48:08 +0700

    update

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.app');
+
+@section('content')
+    <div class="card p-4 shadow p-3 mb-5 bg-body-tertiary rounded">
+
+        <h3>Hallo</h3>
+        <h3>Selamat Datang....</h3>
+        <h2 style="color : #004643">
+            {{ Auth::user()->name }}
+
+        </h2>
+    </div>
+@endsection
